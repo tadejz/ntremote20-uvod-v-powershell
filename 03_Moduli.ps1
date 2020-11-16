6a1d6f82bb82d7895265db66b5103856bf40da44
Tadej Žlak <tadej.zlak@sola-prihodnosti.si>
2020-11-16 12:02:17 +0100

    Dopolnitev Load-Module z možnostjo -Debug, ki ne izvede EXIT, 1 v primeru neobstoja modula. Na predlog Uroša Cesnika

--- 03_Moduli.ps1
+++ 03_Moduli.ps1
@@ -6,47 +6,55 @@
     poenostavim.
 #>
 
-Function Load-Module ($module) {
+Function Load-Module () {
+    param (
+        [Parameter(Mandatory=$true)][string]$Module, # Ime modula
+        [Parameter(Mandatory=$false)][switch]$Debug # Če je $true ne izvede EXIT, 1
+    )
 
     # Če je modul že uvožen, obvesti uporabnika
-    if (Get-Module | Where-Object {$_.Name -eq $module}) {
-        write-host "Modul $module je že uvožen."
+    if (Get-Module | Where-Object {$_.Name -eq $Module}) {
+        write-host "Modul $Module je že uvožen."
     }
     else {
 
         # Če modul ni uvožen, a je na voljo na disku, ga uvozi
-        if (Get-Module -ListAvailable | Where-Object {$_.Name -eq $module}) {
-            Import-Module $module -Verbose
+        if (Get-Module -ListAvailable | Where-Object {$_.Name -eq $Module}) {
+            Import-Module $Module -Verbose
         }
         else {
 
             # Če modul ni uvožen, niti ga ni na disku, a je na voljo v spletni galeriji, ga namesti in uvozi
-            if (Find-Module -Name $module | Where-Object {$_.Name -eq $module}) {
-                Install-Module -Name $module -Force -Verbose -Scope CurrentUser
-                Import-Module $module -Verbose
+            if (Find-Module -Name $Module | Where-Object {$_.Name -eq $Module}) {
+                Install-Module -Name $Module -Force -Verbose -Scope CurrentUser
+                Import-Module $Module -Verbose
             }
             else {
 
                 # Modula ni nikjer. Sorry.
-                write-host "Modul $module ni na voljo, zaključujem."
-                EXIT 1
+                write-host "Modul $Module ni na voljo, zaključujem."
+                if ( $Debug )
+                {
+                    EXIT 1
+                }
             }
         }
     }
 }
 
 <#
-    Uvozimo module
+    Uvozimo module. Če delamo v razhroščevalnem načinu, uporabimo stikalo -Debug, primer:
+    Load-Module -Module "MSOnline" -Debug
 #>
 
 # Najprej osnovni moduli za povezljivost s storitvami
-Load-Module -module "MSOnline"
-Load-Module -module "AzureAD"
-Load-Module -module "ExchangeOnlineManagement"
-Load-Module -module "Microsoft.Online.SharePoint.PowerShell"
-Load-Module -module "SharePointPnPPowerShellOnline"
-Load-Module -module "MicrosoftTeams"
+Load-Module -Module "MSOnline"
+Load-Module -Module "AzureAD"
+Load-Module -Module "ExchangeOnlineManagement"
+Load-Module -Module "Microsoft.Online.SharePoint.PowerShell"
+Load-Module -Module "SharePointPnPPowerShellOnline"
+Load-Module -Module "MicrosoftTeams"
 
 # Dva dodatna modula, zelo uporabna
-Load-Module -module "ImportExcel"
-Load-Module -module "CredentialManager"
+Load-Module -Module "ImportExcel"
+Load-Module -Module "CredentialManager"
